b9a524c744f9537aedeebf1586549547c59ac344
Sanjiv Rana <1291215+eync@users.noreply.github.com>
2023-08-15 15:28:54 +0300

    Added 'Save' and 'Publish' buttons in UI Editor main layer.
    The update included adding two new buttons in the user interface (UI) editor main layer. These buttons will be used respectively for saving the current state of the edited site and for publishing changes. 'Save' and 'Publish' buttons are implemented with functionality that interacts with UPDATE_ENDPOINT and PUBLISH_ENDPOINT, handling network responses and errors. This enhancement improves user experience by providing easy access to important functions directly from the interface.

--- ui-editor-layer/main.js
+++ ui-editor-layer/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const siteLoader = require('./routes/loader');
 const startServer = require('./utils/startServer');
-
+const { PUBLISH_ENDPOINT, UPDATE_ENDPOINT } = require('./config/config');
 const app = express();
 const port = 3000;
 
@@ -10,7 +10,79 @@ app.use(express.static('public'));
 app.use('/loader', siteLoader);
 
 app.get('/', (req, res) => {
-    res.send("<iframe src='/loader' width='1280' height='800'></iframe>");
+   const htmlResponse = `
+    <iframe id="editor-ui-iframe" src="loader" width="1280" height="800"></iframe>
+    <br>
+    <button
+        id="save-button"
+        style="padding: 10px 20px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;">
+        Save
+    </button>
+    <button
+        id="publish-button"
+        style="padding: 10px 20px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;">
+        Publish
+    </button>
+    <script>
+        document.getElementById('save-button').addEventListener('click', async function() {
+            const iframeContent = document.getElementById('editor-ui-iframe').contentWindow.document.body.innerHTML;
+            const embedContent = obj => {
+                const result = {};
+                for (let key in obj) {
+                    result[key] = {
+                        content: obj[key]
+                    };
+                }
+                return result;
+			}
+            const dataToSend = {
+                clientId: 1,
+                contentSections: embedContent(JSON.parse(localStorage.storeObject))
+            };
+			
+            try {
+                const response = await fetch('${UPDATE_ENDPOINT}', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dataToSend)
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+    
+                const jsonResponse = await response.json();
+            } catch (error) {
+                console.error('There was a problem with the fetch operation:', error.message);
+            }
+        });
+        document.getElementById('publish-button').addEventListener('click', async function() {
+            const dataToSend = {
+                clientId: 1
+            };
+            try {
+                const response = await fetch('${PUBLISH_ENDPOINT}', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dataToSend)
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const jsonResponse = await response.json();
+            } catch (error) {
+                console.error('There was a problem with the fetch operation:', error.message);
+            }
+        });
+    </script>
+        `;
+    res.send(htmlResponse);
 });
 
 startServer(app, port);
