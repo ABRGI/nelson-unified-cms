6f1fcd63b2837e0d2c46a0d31c5891b8c048330c
Sanjiv Rana <1291215+eync@users.noreply.github.com>
2023-08-15 15:17:53 +0300

    Extend updateBindings to set 'x-db-map' attribute
    Extended the 'updateBindings' utility to not only set 'x-type' but also 'x-db-map' based on mapped properties. This is useful for applications where 'x-db-map' attribute is required to correspond with database mappings. The 'selectorsWithType' parameter is expanded to include 'dbMap' value, making it easier to update all bindings in one operation. This refactor enhances functionality and efficiency.

--- ui-editor-layer/utils/updateBindings.js
+++ ui-editor-layer/utils/updateBindings.js
@@ -1,12 +1,15 @@
 /**
  * Updates the bindings of elements in the document based on the provided selectors and types.
+ * It sets 'x-type' and 'x-db-map' attributes based on the properties of the mapped objects.
  *
  * @param {Document} document - The document object representing the HTML document.
- * @param {Object.<string, { type: string }>} selectorsWithType - An object mapping selectors to their corresponding types.
- * @returns {Promise<void[]>} A promise that resolves to an array of undefined values.
+ * @param {Object.<string, { message: { type: string, dbMap: string } }>} selectorsWithType - An object mapping selectors to their corresponding types and dbMap values.
+ * @returns {Promise<void[]>} A promise that resolves when all bindings are updated.
  */
 const updateBindings = async (document, selectorsWithType) => {
-    const bindingPromises = Object.entries(selectorsWithType).map(async ([selector, { type }]) => {
+    const { message } = selectorsWithType;
+    console.log(message)
+    const bindingPromises = Object.entries(message).map(async ([selector, { type }]) => {
         const bindings = [...document.querySelectorAll(selector)];
 
         for (const binding of bindings) {
@@ -14,7 +17,15 @@ const updateBindings = async (document, selectorsWithType) => {
         }
     });
 
-    return Promise.all(bindingPromises);
+    const bindingPromisesMap = Object.entries(message).map(async ([selector, { dbMap }]) => {
+        const bindings = [...document.querySelectorAll(selector)];
+
+        for (const binding of bindings) {
+            binding.setAttribute('x-db-map', dbMap);
+        }
+    });
+
+    return Promise.all([bindingPromises, bindingPromisesMap]);
 }
 
 module.exports = { updateBindings };
