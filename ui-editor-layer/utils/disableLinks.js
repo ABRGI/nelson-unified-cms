8e04c30f7ddbdd6d080faa111ecd9780b3a2e8e9
Sanjiv Rana <1291215+eync@users.noreply.github.com>
2023-08-15 15:25:06 +0300

    Enhance link disabling functionality in UI editor
    Refined the 'disableLinks' utility in the UI editor by extending the disabling from just clickable links to any element with 'src' or 'href' attributes, improving security and accessibility. Moreover, elements with the class 'accordion-header w-inline-block' are now replaced with a div element, ensuring smooth document interaction irrespective of the element nature.

--- ui-editor-layer/utils/disableLinks.js
+++ ui-editor-layer/utils/disableLinks.js
@@ -1,21 +1,27 @@
 /**
- * Disables the click events on all links in the given document.
+ * Disables the click events on all elements with 'href' or 'src' attributes in the given document.
+ * For elements with the class 'accordion-header w-inline-block', they are replaced with a div element.
  *
  * @param {Document} document - The document object representing the HTML document.
- * @returns {Promise<HTMLAnchorElement[]>} An array of disabled link elements.
+ * @returns {HTMLAnchorElement[]|HTMLImageElement[]} An array of disabled elements with 'href' or 'src' attributes.
  */
 const disableLinks = async (document) => {
     /**
      * @type {NodeListOf<HTMLAnchorElement|HTMLImageElement>} links
      */
     const links = [...document.querySelectorAll('[href], [src]')];
-
     links.forEach(a => {
+        if (a.className === 'accordion-header w-inline-block') {
+            const div = document.createElement("div");
+            div.innerHTML = a.innerHTML
+            a.replaceWith(div);
+        }
         /**
          * Disable click events on the link element.
          * @type {CSSStyleDeclaration}
          */
-        a.style.pointerEvents = 'none'; });
+        a.style.pointerEvents = 'none';
+    });
 
     return links;
 }
